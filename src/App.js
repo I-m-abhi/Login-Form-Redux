1a2183327da31307a292b7e980e88d327a302598
abhishek <abhisasa2001@gmail.com>
2022-09-15 13:43:07 +0530

    loginform with redux

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter,Routes,Route} from 'react-router-dom';
+import Welcome from './components/Welcome';
+import Login from './components/Login';
+import About from './components/About';
+import Register from './components/Register';
+import ProtectedRoute from './components/ProtectedRoute';
+import ProtectedLogReg from './components/ProtectedLogReg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <>
+   <BrowserRouter>
+     <Routes>
+     <Route element={<ProtectedRoute />}>
+          <Route path="/" element={<Welcome />}></Route>
+          <Route path="/about" element={<About />}></Route>
+        </Route>
+
+        <Route element={<ProtectedLogReg />}>
+          <Route path="/login" element={<Login />}></Route>
+          <Route path="/register" element={<Register />}></Route>
+        </Route>
+     </Routes>
+   </BrowserRouter>
+   </>
   );
 }
 
